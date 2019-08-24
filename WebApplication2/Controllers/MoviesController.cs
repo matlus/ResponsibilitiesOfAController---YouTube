738a20203a8c7a7ddbc6dd55a499a2a83d926389
Shiv Kumar <shiv.kmr003@gmail.com>
2019-08-24 17:56:56 -0400

    Add project files.

---
+++ WebApplication2/Controllers/MoviesController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication2.DomainLayer;
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly DomainFacade _domainFacade = new DomainFacade();
+        // GET api/movies
+        [HttpGet]
+        public IEnumerable<MovieResource> Get()
+        {
+            var movies = _domainFacade.GetAllMovies();
+            return MapToMovieResource(movies);
+
+        }
+
+        // GET api/genre/Action
+        [HttpGet("genre/{genreAsString}")]
+        public ActionResult<IEnumerable<MovieResource>> GetMoviesByGenre(string genreAsString)
+        {
+            try
+            {
+                var genre = GenreParser.Parse(genreAsString);
+                var movies = _domainFacade.GetMoviesByGenre(genre);
+                return new OkObjectResult(MapToMovieResource(movies));
+            }
+            catch (Exception e)
+            {
+                return new ExceptionActionResult(e);
+            }
+        }
+
+        private static IEnumerable<MovieResource> MapToMovieResource(IEnumerable<Movie> movies)
+        {
+            foreach (var movie in movies)
+            {
+                yield return new MovieResource { Name = movie.Name, Genre = GenreParser.ToString(movie.Genre), ImageUrl = movie.ImageUrl, Year = movie.Year };
+            }
+        }
+    }
+}
